2766a4cbfffce47c8cba18639d14f86b2b37f3a9
Alexis Svinartchouk <alexis@resin.io>
2020-12-22 18:47:47 +0100

    Keep fan-control service running during screensaver
    Change-type: patch

--- screensaver_off.sh
+++ screensaver_off.sh
@@ -6,4 +6,3 @@ if [ ! -d /sys/class/gpio/gpio131 ]; then echo 131 > /sys/class/gpio/export; fi
 if [ -d /sys/class/gpio/gpio131 ]; then echo 0 > /sys/class/gpio/gpio131/value; fi
 if [ -d /sys/class/pwm/pwmchip2/pwm0 ]; then echo 1 > /sys/class/pwm/pwmchip2/pwm0/enable; fi
 dbus-send --system --dest=org.freedesktop.systemd1 --type=method_call --print-reply /org/freedesktop/systemd1 org.freedesktop.systemd1.Manager.StartUnit string:gov-switch.service string:replace
-dbus-send --system --dest=org.freedesktop.systemd1 --type=method_call --print-reply /org/freedesktop/systemd1 org.freedesktop.systemd1.Manager.StartUnit string:fan-control.service string:replace 
--- screensaver_on.sh
+++ screensaver_on.sh
@@ -7,17 +7,4 @@ if [ -d /sys/class/gpio/gpio131 ]; then echo 1 > /sys/class/gpio/gpio131/value; 
 if [ -d /sys/class/pwm/pwmchip2/pwm0 ]; then echo 0 > /sys/class/pwm/pwmchip2/pwm0/enable; fi
 dbus-send --system --dest=org.freedesktop.systemd1 --type=method_call --print-reply /org/freedesktop/systemd1 org.freedesktop.systemd1.Manager.StopUnit string:gov-switch.service string:replace
 echo powersave > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-dbus-send --system --dest=org.freedesktop.systemd1 --type=method_call --print-reply /org/freedesktop/systemd1 org.freedesktop.systemd1.Manager.StopUnit string:fan-control.service string:replace
-for i in $(seq 1 -1 0); do
-	if [ ! -d /sys/class/pwm/pwmchip1/pwm0 ]; then
-		echo 0 > /sys/class/pwm/pwmchip1/export;
-	fi
-	if [ -d /sys/class/pwm/pwmchip1/pwm0 ]; then
-		echo 45000 > /sys/class/pwm/pwmchip1/pwm0/period
-		echo 45000 > /sys/class/pwm/pwmchip1/pwm0/duty_cycle;
-	fi
-	sleep 60;
-done
-if [ ! -d /sys/class/pwm/pwmchip1/pwm0 ]; then echo 0 > /sys/class/pwm/pwmchip1/export; fi
-if [ -d /sys/class/pwm/pwmchip1/pwm0 ]; then echo 0 > /sys/class/pwm/pwmchip1/pwm0/duty_cycle && echo 1 > /sys/class/pwm/pwmchip1/pwm0/period; fi
 clicklock
