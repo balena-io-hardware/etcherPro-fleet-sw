19f21064c3f97c888243606f476c40bbd7c89a8b
Alexis Svinartchouk <alexis@resin.io>
2020-12-22 17:49:02 +0100

    Do not turn cpus 1 - 3 off during the screensaver
    If we do, nothing gets scheduled on them when we turn them back on
    
    Change-type: patch

--- screensaver_on.sh
+++ screensaver_on.sh
@@ -5,9 +5,6 @@ if [ -d /sys/class/gpio/gpio101 ]; then echo 0 > /sys/class/gpio/gpio101/value; 
 if [ ! -d /sys/class/gpio/gpio131 ]; then echo 131 > /sys/class/gpio/export; fi
 if [ -d /sys/class/gpio/gpio131 ]; then echo 1 > /sys/class/gpio/gpio131/value; fi
 if [ -d /sys/class/pwm/pwmchip2/pwm0 ]; then echo 0 > /sys/class/pwm/pwmchip2/pwm0/enable; fi
-echo 0 > /sys/devices/system/cpu/cpu1/online
-echo 0 > /sys/devices/system/cpu/cpu2/online
-echo 0 > /sys/devices/system/cpu/cpu3/online
 dbus-send --system --dest=org.freedesktop.systemd1 --type=method_call --print-reply /org/freedesktop/systemd1 org.freedesktop.systemd1.Manager.StopUnit string:gov-switch.service string:replace
 echo userspace > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 echo powersave > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
