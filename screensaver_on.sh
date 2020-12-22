b322d2050ed0d2818699b16fcec11c3aea202fd1
Alexis Svinartchouk <alexis@resin.io>
2020-12-22 18:43:31 +0100

    Remove unneded userspace cpufreq governor setting
    Change-type: patch

--- screensaver_on.sh
+++ screensaver_on.sh
@@ -6,7 +6,6 @@ if [ ! -d /sys/class/gpio/gpio131 ]; then echo 131 > /sys/class/gpio/export; fi
 if [ -d /sys/class/gpio/gpio131 ]; then echo 1 > /sys/class/gpio/gpio131/value; fi
 if [ -d /sys/class/pwm/pwmchip2/pwm0 ]; then echo 0 > /sys/class/pwm/pwmchip2/pwm0/enable; fi
 dbus-send --system --dest=org.freedesktop.systemd1 --type=method_call --print-reply /org/freedesktop/systemd1 org.freedesktop.systemd1.Manager.StopUnit string:gov-switch.service string:replace
-echo userspace > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 echo powersave > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 dbus-send --system --dest=org.freedesktop.systemd1 --type=method_call --print-reply /org/freedesktop/systemd1 org.freedesktop.systemd1.Manager.StopUnit string:fan-control.service string:replace
 for i in $(seq 1 -1 0); do
