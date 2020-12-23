124486a1f852cb5f17f72977551c1c8e7391941f
Alexis Svinartchouk <alexis@resin.io>
2020-12-23 13:29:36 +0100

    Add drives order for EtcherPro 2.3.2 (same as 2.3.1)
    Change-type: patch

--- update-config-and-start.js
+++ update-config-and-start.js
@@ -11,6 +11,25 @@ const {
 
 const CONFIG_FILE_PATH = '/root/.config/balena-etcher/config.json';
 
+const drivesOrder2_3_x = [
+	'platform-33800000.pcie-pci-0000:01:00.0-usb-0:2:1.0-scsi-0:0:0:0',
+	'platform-xhci-hcd.1.auto-usb-0:1.7:1.0-scsi-0:0:0:0',
+	'platform-xhci-hcd.0.auto-usb-0:1.7:1.0-scsi-0:0:0:0',
+	'platform-xhci-hcd.1.auto-usb-0:1.5:1.0-scsi-0:0:0:0',
+	'platform-xhci-hcd.0.auto-usb-0:1.5:1.0-scsi-0:0:0:0',
+	'platform-xhci-hcd.1.auto-usb-0:1.4:1.0-scsi-0:0:0:0',
+	'platform-xhci-hcd.0.auto-usb-0:1.4:1.0-scsi-0:0:0:0',
+	'platform-xhci-hcd.1.auto-usb-0:1.3:1.0-scsi-0:0:0:0',
+	'platform-xhci-hcd.0.auto-usb-0:1.3:1.0-scsi-0:0:0:0',
+	'platform-xhci-hcd.1.auto-usb-0:1.2:1.0-scsi-0:0:0:0',
+	'platform-xhci-hcd.0.auto-usb-0:1.2:1.0-scsi-0:0:0:0',
+	'platform-xhci-hcd.1.auto-usb-0:1.1:1.0-scsi-0:0:0:0',
+	'platform-xhci-hcd.0.auto-usb-0:1.1:1.0-scsi-0:0:0:0',
+	'platform-33800000.pcie-pci-0000:01:00.0-usb-0:1:1.0-scsi-0:0:0:0',
+	'platform-33800000.pcie-pci-0000:01:00.0-usb-0:4:1.0-scsi-0:0:0:0',
+	'platform-33800000.pcie-pci-0000:01:00.0-usb-0:3:1.0-scsi-0:0:0:0',
+];
+
 const db = {
 	default: {
 		autoSelectAllDrives: true,
@@ -60,24 +79,12 @@ const db = {
 	},
 	'2.3.1': {
 		default: {
-			drivesOrder: [
-				'platform-33800000.pcie-pci-0000:01:00.0-usb-0:2:1.0-scsi-0:0:0:0',
-				'platform-xhci-hcd.1.auto-usb-0:1.7:1.0-scsi-0:0:0:0',
-				'platform-xhci-hcd.0.auto-usb-0:1.7:1.0-scsi-0:0:0:0',
-				'platform-xhci-hcd.1.auto-usb-0:1.5:1.0-scsi-0:0:0:0',
-				'platform-xhci-hcd.0.auto-usb-0:1.5:1.0-scsi-0:0:0:0',
-				'platform-xhci-hcd.1.auto-usb-0:1.4:1.0-scsi-0:0:0:0',
-				'platform-xhci-hcd.0.auto-usb-0:1.4:1.0-scsi-0:0:0:0',
-				'platform-xhci-hcd.1.auto-usb-0:1.3:1.0-scsi-0:0:0:0',
-				'platform-xhci-hcd.0.auto-usb-0:1.3:1.0-scsi-0:0:0:0',
-				'platform-xhci-hcd.1.auto-usb-0:1.2:1.0-scsi-0:0:0:0',
-				'platform-xhci-hcd.0.auto-usb-0:1.2:1.0-scsi-0:0:0:0',
-				'platform-xhci-hcd.1.auto-usb-0:1.1:1.0-scsi-0:0:0:0',
-				'platform-xhci-hcd.0.auto-usb-0:1.1:1.0-scsi-0:0:0:0',
-				'platform-33800000.pcie-pci-0000:01:00.0-usb-0:1:1.0-scsi-0:0:0:0',
-				'platform-33800000.pcie-pci-0000:01:00.0-usb-0:4:1.0-scsi-0:0:0:0',
-				'platform-33800000.pcie-pci-0000:01:00.0-usb-0:3:1.0-scsi-0:0:0:0',
-			],
+			drivesOrder: drivesOrder2_3_x,
+		},
+	},
+	'2.3.2': {
+		default: {
+			drivesOrder: drivesOrder2_3_x,
 		},
 	},
 }
