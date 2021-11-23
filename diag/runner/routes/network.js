aa977a64198cdbd8c9bdd8eaef73b7caa55f12c5
Peter Makra <6892971+mcraa@users.noreply.github.com>
2021-11-23 16:55:42 +0100

    patch: [diag] save history to filesystem
    Change-Type: patch

--- diag/runner/routes/network.js
+++ diag/runner/routes/network.js
@@ -2,11 +2,38 @@ var express = require('express');
 var os = require('os')
 var router = express.Router();
 
+var DiagHistory = require('../services/DiagResult').DiagHistory
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const networks = os.networkInterfaces();
 
+  DiagHistory
+    .createNetworkResult
+    .withData(networks)
+    .persist()
+
   res.json(networks);
 });
+
+router.get('/results/list', (req, res, next) => {
+  let entries = DiagHistory.listNetworkForDate()
+
+  res.json(entries)
+})
+
+router.get('/results/list/:date', (req, res, next) => {
+  const { date } = req.params
+  let entries = DiagHistory.listNetworkForDate(date)
+
+  res.json(entries)
+})
+
+router.get('/results/view/:fileName', (req, res, next) => {
+  const { fileName } = req.params
+  let content = DiagHistory.readNetworkFile(fileName)
+
+  res.send(content)
+})
 
 module.exports = router;
---
+++ diag/runner/services/DiagResult.js
@@ -0,0 +1,110 @@
+const fs = require('fs');
+const path = require('path');
+
+class DiagResult {
+    _name;
+    _path;
+    _data;
+
+    constructor(name, filepath) {
+        this._name = name
+        this._path = filepath ? filepath : path.join(__dirname, '..', 'diag-data', 'history', name)
+    }
+
+    #createDateFolder = (date) => `${date.getFullYear()}_${date.getMonth()}_${date.getDay()}`;
+
+    withData(data) {
+        if (this._data) {
+            this._data = `${this._data}\n\n${JSON.stringify(data)}`
+        } else {
+            this._data = JSON.stringify(data);
+        }
+
+        return this;
+    }
+
+    persist() {
+        let now = new Date(Date.now())
+        let folderPath = path.join(this._path, this.#createDateFolder(now))
+
+        if (!fs.existsSync(folderPath)) {
+            fs.mkdirSync(folderPath, { recursive: true })
+        }
+        fs.writeFileSync(
+            path.join(folderPath, `${now.getTime()}.bin`),
+            this._data,
+        )
+
+        return this;
+    }
+
+    list(slug) {
+        let currentPath = this._path;
+        if (slug) {
+            currentPath = path.join(this._path, slug)
+        }  
+
+        console.log(this, currentPath)
+
+        try {
+            const res = fs.readdirSync(currentPath)
+    
+            return res;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    read(fileName) {
+        if (!fileName) return;
+
+        try {            
+            const fileDate = new Date(parseInt(fileName.split(".")[0]))
+            let folderName = this.#createDateFolder(fileDate);
+            let currentPath = path.join(this._path, folderName, fileName)
+            
+            const res = fs.readFileSync(currentPath)
+            
+            return res;
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+const DiagTypes = {
+    NETWORK: 'network',
+    DRIVES: 'drives',
+    LEDS: 'leds'
+}
+
+const createDiagResult = (diagType) => new DiagResult(diagType)
+const createNetworkResult =  createDiagResult(DiagTypes.NETWORK)
+const createLedsResult = createDiagResult(DiagTypes.LEDS)
+const createDrivesResult = createDiagResult(DiagTypes.DRIVES)
+
+const listNetworkForDate = (date) => createDiagResult(DiagTypes.NETWORK).list(date)
+const listLedsForDate = (date) => createDiagResult(DiagTypes.LEDS).list(date)
+const listDrivesForDate = (date) => createDiagResult(DiagTypes.DRIVES).list(date)
+
+const readNetworkFile = (fileName) => createDiagResult(DiagTypes.NETWORK).read(fileName)
+const readLedsFile = (fileName) => createDiagResult(DiagTypes.LEDS).read(fileName)
+const readDrivesFile = (fileName) => createDiagResult(DiagTypes.DRIVES).read(fileName)
+
+
+const DiagHistory = {
+    createDrivesResult,
+    createLedsResult,
+    createNetworkResult,
+    listNetworkForDate,
+    listLedsForDate,
+    listDrivesForDate, 
+    readNetworkFile,
+    readLedsFile,
+    readDrivesFile 
+}     
+
+module.exports = { 
+    DiagResult,
+    DiagHistory
+}
