a6410a35223915b3587a3fa2877709441b16a988
Peter Makra <6892971+mcraa@users.noreply.github.com>
2021-11-24 20:33:51 +0100

    patch: [diag] refactored runCheck
    Change-Type: patch

--- diag/runner/ui/src/App.tsx
+++ diag/runner/ui/src/App.tsx
@@ -1,114 +1,95 @@
-import React, { useEffect, useState } from 'react';
-import { 
-  Accordion,
-  Box,
-  Button,
-  Link,
-  Navbar,
-} from 'rendition';
-import { 
-  faTimes, 
-  faCheckCircle, 
-  faQuestionCircle, 
-  faTimesCircle 
-} from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Switch, Route, Link as RouterLink, useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Accordion, Box, Button, Link, Navbar } from "rendition";
+import {
+  faTimes,
+  faCheckCircle,
+  faQuestionCircle,
+  faTimesCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  Switch,
+  Route,
+  Link as RouterLink,
+  useLocation,
+} from "react-router-dom";
 
-import './App.css';
-import { ScreenTest } from './pages/ScreenTest'
-import { DiagSteps } from './pages/DiagSteps';
-import { Leds } from './pages/Leds';
-import { Drives } from './pages/Drives';
-import { NetworkInfo } from './pages/NetworkInfo';
+import "./App.css";
+import { ScreenTest } from "./pages/ScreenTest";
+import { DiagSteps } from "./pages/DiagSteps";
+import { Leds } from "./pages/Leds";
+import { Drives } from "./pages/Drives";
+import { NetworkInfo } from "./pages/NetworkInfo";
+import {
+  runChecks,
+  DiagnosticsState,
+  ExpectationsDict,
+} from "./services/ExpectCheck";
 
 function App() {
   const [showScreen, setShowScreen] = useState(false);
-  const [checkState, setCheckState] = useState("notrun" as "notrun" | "ok" | "failed")
-  const [checkErrors, setCheckErrors] = useState([] as Array<string>)
-  const [expects, setExpects] = useState({} as { [index: string]: Array<{ method: string, op: string, value: string | number }> });
-  const location = useLocation()
+  const [checkState, setCheckState] = useState(
+    "notrun" as "notrun" | "ok" | "failed"
+  );
+  const [checkErrors, setCheckErrors] = useState([] as Array<string>);
+  const [expects, setExpects] = useState<ExpectationsDict>({});
+  const location = useLocation();
 
   useEffect(() => {
-    getExpects()
-  }, [])
+    getExpects();
+  }, []);
 
   const closeScreenFrame = () => {
-    setShowScreen(false);  
-  }
+    setShowScreen(false);
+  };
 
   const openScreenFrame = () => {
-    setShowScreen(true);  
-  }
+    setShowScreen(true);
+  };
 
-  
   const getExpects = async () => {
-    let raw = await fetch(`/expects/expects.json`)
-    setExpects(JSON.parse(await raw.text()))
-  }
+    let raw = await fetch(`/expects/expects.json`);
+    const exp = JSON.parse(await raw.text());
+    setExpects(exp);
 
-  const eq = (a: any, b: any) => a === b
-  const ne = (a: any, b: any) => a !== b
-  const gt = (a: any, b: any) => a > b
-  const lt = (a: any, b: any) => a < b
+    return exp;
+  };
 
-  const getMethod = (propString: string, obj: any) => {
-    if (!propString)
-    return obj;
-
-    var prop, props = propString.split('.');
-
-    for (var i = 0, iLen = props.length - 1; i < iLen; i++) {
-      prop = props[i];
-
-      var candidate = obj[prop];
-      if (candidate !== undefined) {
-        obj = candidate;
-      } else {
-        break;
-      }
-    }
-    return obj[props[i]];
-  }
-
-  const runChecks = () => {
+  const doCheck = async () => {
     setCheckErrors([]);
-    // let state = {
-    //   leds
-    // } as { [index: string]: any}
+    const exp = await getExpects();
 
-    // let operators = {
-    //   eq,
-    //   ne,
-    //   gt,
-    //   lt,
-    // } as { [index: string] : any }
+    // TODO: remove test data
+    let state: DiagnosticsState = {
+      leds: ["led0", "led0", "led0", "led0"],
+      drives: ["led0", "led0", "led0", "led0"],
+    } as DiagnosticsState;
 
-    // let hasFail = Object.keys(expects).map(k => {
-    //   return expects[k].map(m => {        
-    //     if (!operators[m.op](getMethod(m.method, state[k]), m.value)) {
-    //       setCheckState('failed');
-    //       setCheckErrors([...checkErrors, `${k}: ${m.method} expected to be ${m.op} ${m.value} but was ${getMethod(m.method, state[k])}`])
-    //       return false;
-    //     }
-    //     return true;
-    //   }).some(success => !success)
-    // }).some(hasFail => hasFail)
+    let { success, errors } = runChecks(state, exp);
 
-    // if (!hasFail) { setCheckState('ok') }
-  }
+    if (success) {
+      setCheckState("ok");
+    } else {
+      setCheckState("failed");
+      setCheckErrors(errors);
+    }
+  };
 
   const burnSerial = () => {
-    console.log('burrn');    
-  }
+    console.log("burrn");
+  };
 
   return (
     <div className="App">
-      {location && location.pathname.startsWith("/manual") ? 
+      {location && location.pathname.startsWith("/manual") ? (
         <Navbar
-          brand={<RouterLink to="/"><Link color="white">HW Diag</Link></RouterLink>}
+          brand={
+            <RouterLink to="/">
+              <Link color="white">HW Diag</Link>
+            </RouterLink>
+          }
           color="white"
-          >
+        >
           <RouterLink to="/manual/expectations">
             <Link color="white">Expectations</Link>
           </RouterLink>
@@ -118,83 +99,163 @@ function App() {
           <RouterLink to="/diagsteps/start">
             <Link color="white">Diag steps</Link>
           </RouterLink>
-        </Navbar> : <></>
-      }
-        <Switch>
-          <Route path="/diagsteps">
-            <DiagSteps />
-          </Route>
-          <Route path="/screen">
-            <ScreenTest />
-          </Route>
-          <Route path="/manual/expectations">
-              <ul>
-                {Object.keys(expects).map(k => <>
-                  <li>
-                    {k}: <ol>{expects[k].map(e => <li>[{e.method}] is '{e.op}' <b>{e.value}</b></li>)}</ol>
-                  </li>
-                </>)}
-              </ul>
-          </Route>
-          <Route path="/manual">
-                <Accordion items={[
-                  {
-                    label: 'Screen',
-                    panel: 
-                      <Box>
-                        <Button onClick={() => openScreenFrame()}>Open screen test</Button>
-                      </Box>
-                  },
-                  {
-                    label: 'Leds',
-                    panel: <Leds />                      
-                  },
-                  {
-                    label: 'Write speed',
-                    panel: 
-                      <>
-                        <Drives />
-                      </>
-                  },
-                  {
-                    label: 'Network',
-                    panel: 
-                      <>
-                        <NetworkInfo />
-                      </>
-                  },
-                  {
-                    label: 'Mark test complete',
-                    panel:
-                      <Box>
-                        <Button onClick={() => runChecks()}>Check expectations</Button>&nbsp;
-                        {checkState === 'ok' ? <Button success confirmation={{ placement: 'top', text: 'Are you sure?' }} onClick={() => burnSerial()} icon={<FontAwesomeIcon color='green' icon={faCheckCircle} />}>Burn serial</Button> : <></> }
-                        {checkState === 'failed' ? <Button danger icon={<FontAwesomeIcon color='red' icon={faTimesCircle} />}>Burn serial</Button> : <></> }
-                        {checkState === 'notrun' ? <Button disabled icon={<FontAwesomeIcon color='gray' icon={faQuestionCircle} />}>Burn serial</Button> : <></> }
-                        {checkErrors.length ? 
+        </Navbar>
+      ) : (
+        <></>
+      )}
+      <Switch>
+        <Route path="/diagsteps">
+          <DiagSteps />
+        </Route>
+        <Route path="/screen">
+          <ScreenTest />
+        </Route>
+        <Route path="/manual/expectations">
+          <ul>
+            {Object.keys(expects).map((k) => (
+              <>
+                <li>
+                  {k}:{" "}
+                  <ol>
+                    {expects[k].map((e) => (
+                      <li>
+                        [{e.method}] is '{e.op}' <b>{e.value}</b>
+                      </li>
+                    ))}
+                  </ol>
+                </li>
+              </>
+            ))}
+          </ul>
+        </Route>
+        <Route path="/manual">
+          <Accordion
+            items={
+              [
+                {
+                  label: "Screen",
+                  panel: (
+                    <Box>
+                      <Button onClick={() => openScreenFrame()}>
+                        Open screen test
+                      </Button>
+                    </Box>
+                  ),
+                },
+                {
+                  label: "Leds",
+                  panel: <Leds />,
+                },
+                {
+                  label: "Write speed",
+                  panel: (
+                    <>
+                      <Drives />
+                    </>
+                  ),
+                },
+                {
+                  label: "Network",
+                  panel: (
+                    <>
+                      <NetworkInfo />
+                    </>
+                  ),
+                },
+                {
+                  label: "Mark test complete",
+                  panel: (
+                    <Box>
+                      <Button onClick={() => doCheck()}>
+                        Check expectations
+                      </Button>
+                      &nbsp;
+                      {checkState === "ok" ? (
+                        <Button
+                          success
+                          confirmation={{
+                            placement: "top",
+                            text: "Are you sure?",
+                          }}
+                          onClick={() => burnSerial()}
+                          icon={
+                            <FontAwesomeIcon
+                              color="green"
+                              icon={faCheckCircle}
+                            />
+                          }
+                        >
+                          Burn serial
+                        </Button>
+                      ) : (
+                        <></>
+                      )}
+                      {checkState === "failed" ? (
+                        <Button
+                          danger
+                          icon={
+                            <FontAwesomeIcon color="red" icon={faTimesCircle} />
+                          }
+                        >
+                          Burn serial
+                        </Button>
+                      ) : (
+                        <></>
+                      )}
+                      {checkState === "notrun" ? (
+                        <Button
+                          disabled
+                          icon={
+                            <FontAwesomeIcon
+                              color="gray"
+                              icon={faQuestionCircle}
+                            />
+                          }
+                        >
+                          Burn serial
+                        </Button>
+                      ) : (
+                        <></>
+                      )}
+                      {checkErrors.length ? (
                         <ol>
-                          {checkErrors.map(e => <li>{e}</li>)}
+                          {checkErrors.map((e) => (
+                            <li>{e}</li>
+                          ))}
                         </ol>
-                        : <></>}
-                      </Box>
-                  }
-                ] as any} 
-                />
-              {showScreen ? <>
-                <Button 
-                  primary
-                  onClick={() => closeScreenFrame()}
-                  className="add-fab"
-                  padding='13px'
-                  style={{ borderRadius: '100%'}}
-                  width={23}
-                  icon={<FontAwesomeIcon icon={faTimes}/>}
-                  />
-                <iframe className="App-frame" src={`/screen`} title='screen' key="screen-frame"></iframe>
-              </> : <></>}
-          </Route>
-        </Switch>       
-    </div>        
+                      ) : (
+                        <></>
+                      )}
+                    </Box>
+                  ),
+                },
+              ] as any
+            }
+          />
+          {showScreen ? (
+            <>
+              <Button
+                primary
+                onClick={() => closeScreenFrame()}
+                className="add-fab"
+                padding="13px"
+                style={{ borderRadius: "100%" }}
+                width={23}
+                icon={<FontAwesomeIcon icon={faTimes} />}
+              />
+              <iframe
+                className="App-frame"
+                src={`/screen`}
+                title="screen"
+                key="screen-frame"
+              ></iframe>
+            </>
+          ) : (
+            <></>
+          )}
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
