a311e2d6ff2192ddbcb58c594e5006546f804e22
Peter Makra <6892971+mcraa@users.noreply.github.com>
2021-11-25 18:53:23 +0100

    patch: [diag] added swagger for clarity

--- diag/runner/app.js
+++ diag/runner/app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 var indexRouter = require('./routes/index');
 var ledsRouter = require('./routes/leds');
@@ -9,6 +11,7 @@ var pingRouter = require('./routes/ping');
 var drivesRouter = require('./routes/drives');
 var supervisorRouter = require('./routes/supervisor');
 var networkRouter = require('./routes/network');
+var expectsRouter = require('./routes/expects');
 
 var app = express();
 
@@ -23,11 +26,14 @@ app.use('/api/ping', pingRouter);
 app.use('/api/drives', drivesRouter);
 app.use('/api/supervisor', supervisorRouter);
 app.use('/api/network', networkRouter);
+app.use('/api/expects', expectsRouter);
+
+// docs
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // serve the UI
 app.use(express.static(path.resolve(__dirname, './ui/build')));
 app.get('*', (_, res) => {
   res.sendFile(path.resolve(__dirname, './ui/build', 'index.html'));
 });
-
 module.exports = app;
