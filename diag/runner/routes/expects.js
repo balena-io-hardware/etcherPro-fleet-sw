076b691826a87df089c062a84c0f9d73989efc4d
Peter Makra <6892971+mcraa@users.noreply.github.com>
2021-11-26 15:34:32 +0100

    patch: [diag] implemented expectation endpoints; updated ui; added default expect to dockerfile

--- diag/runner/routes/expects.js
+++ diag/runner/routes/expects.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path')
+var fs = require('fs')
 
-/* GET home page. */
+/* GET view expectations */
 router.get('/', function(req, res, next) {
-  res.sendStatus(403);
+  try {
+    const data = fs.readFileSync(path.join('/usr', "src", "diag-data", "expects.json"), 'utf8')
+    res.json(data)
+  } catch (err) {
+    console.error(err)
+    res.sendStatus(501);
+  }
+});
+
+/* POST set expectations */
+router.post('/', function(req, res, next) {
+  try {
+    fs.writeFileSync(path.join('/usr', "src", "diag-data", "expects.json"), JSON.stringify(req.body))
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error)
+    res.sendStatus(501);
+  }  
 });
 
 module.exports = router;
