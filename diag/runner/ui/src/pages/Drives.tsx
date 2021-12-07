c15e4d357bc15fa0d2143a839c68b9746a8d59c5
Peter Makra <6892971+mcraa@users.noreply.github.com>
2021-12-07 16:43:17 +0100

    patch: [diag] added 1-by-1 fio calls and leds to drives
    Change-Type: patch

--- diag/runner/ui/src/pages/Drives.tsx
+++ diag/runner/ui/src/pages/Drives.tsx
@@ -1,25 +1,40 @@
 import { useEffect, useState } from 'react';
-import { Box, Button, HighlightedName, Table, Txt } from 'rendition'
+import { Box, Button, HighlightedName, ProgressBar, Table, Txt } from 'rendition'
 import { FioResult } from '../iterfaces/FioResult';
+import { LedService } from '../services/Leds'
 
 type DrivesPageProps = {
   onDataReceived?: (data: any) => void
   autoload?: boolean
 }
 
-type DrivesList = {
+type DrivesListItem = {
   path: string //--> /dev/disk/by-path/*
-  device: string //--> /dev/sd[.]
+  device: string //--> sd[.] (no /dev at the beginning)
+}
+
+type DriveLeds = {
+  [index: string]: string[]
+}
+
+type ToggleLeds = {
+  [index: string]: boolean
 }
 
 export const Drives = ({ autoload, onDataReceived }: DrivesPageProps) => {
-  const [drives, setDrives] = useState([] as Array<DrivesList>);
+  const [drives, setDrives] = useState([] as Array<DrivesListItem>);
   const [fioCallStatus, setFioCallStatus] = useState<"none" | "ok" | "fail" | "inprogress">("none");
   const [fioResults, setFioResults] = useState<FioResult[]>([]);
+  const [driveLeds, setDriveLeds] = useState<DriveLeds>({});
+  const [toggleLeds, setToggleLeds] = useState<ToggleLeds>({});
+  const [fioProgress, setFioProgress] = useState<number>(0)
 
   useEffect(() => {
     if (autoload) {
-      (async () => await getDrives())()
+      (async () => {
+        await getDrives()
+        await getDriveLeds()
+      })()
     }
   }, [autoload])
 
@@ -31,21 +46,28 @@ export const Drives = ({ autoload, onDataReceived }: DrivesPageProps) => {
       onDataReceived({ devices: drivesResponse })
     }
   } 
+
+  const getDriveLeds = async () => {
+    const res = await fetch(`/api/supervisor/etcher-config`)
+    const configResponse = await res.json()
+    setDriveLeds(configResponse['ledsMapping']);
+  }
   
-  const callFioRun = async () => {
+  const callFioRunAll = async () => {
     if (fioCallStatus !== 'none') {
       await getDrives();
     }
 
     setFioCallStatus("inprogress");
+    setFioProgress(0);
+    let progressTime = setInterval(() => setFioProgress(fioProgress+5), 2100)
 
     try {
-      let devices = drives.map(d => d.path)
+      let devices = drives.map(d => `/dev/${d.device}`)
       const fioRun = await fetch(`/api/drives/fio`, { 
         method: 'POST',
         body: JSON.stringify({ 
           devices: devices, 
-          bs: "1m", 
           invalidate: 1,
           overwrite: 1
         }),
@@ -68,6 +90,59 @@ export const Drives = ({ autoload, onDataReceived }: DrivesPageProps) => {
     } catch (error) {
       setFioCallStatus("fail")
     }
+
+    clearInterval(progressTime)
+  }
+
+  const callFioOneByOne = async () => {
+    setFioCallStatus("inprogress");
+    setFioProgress(0);
+
+    for (let deviceItem of drives) {
+      const fioRun = await fetch(`/api/drives/fio`, { 
+        method: 'POST',
+        body: JSON.stringify({ 
+          devices: `/dev/${deviceItem.path}`, 
+          invalidate: 1,
+          overwrite: 1
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      })
+      
+      if (fioRun.ok) {  
+        setFioProgress(fioProgress + (100 / drives.length) ) 
+
+        let fioRes = await fetch('/api/drives/fio/last')
+        const lastRes = await fioRes.json()
+        
+        setFioResults([...fioResults, lastRes])
+        
+        if (onDataReceived) {
+          onDataReceived({ devices: drives, results: lastRes })
+        }
+      } 
+    }
+
+    // check if all the calls were good
+    if (fioProgress < 99) {
+      setFioCallStatus("fail")
+    } else {
+      setFioCallStatus("ok")
+    }
+  }
+
+  const handleResultClick = async (device: string) => {
+    let driveIndex = drives.findIndex(d => d.device === device)
+    let led_blue = driveLeds[drives[driveIndex].path][2] // led.*_b
+    if (toggleLeds[drives[driveIndex].path]) {
+      setToggleLeds({ ...toggleLeds, [drives[driveIndex].path]: false })
+      await LedService.callOneLed(led_blue, "0")
+    } else {
+      setToggleLeds({ ...toggleLeds, [drives[driveIndex].path]: true })
+      await LedService.callOneLed(led_blue, "99")
+    }
   }
 
   return (
@@ -84,18 +159,29 @@ export const Drives = ({ autoload, onDataReceived }: DrivesPageProps) => {
           danger={fioCallStatus === "fail"} 
           success={fioCallStatus === "ok"} 
           disabled={fioCallStatus === "inprogress"}
-          onClick={() => callFioRun()}
+          onClick={() => callFioRunAll()}
         >
-          Run fio
+          Run fio all
         </Button>
-        <Txt italic>Takes about 30 seconds</Txt>
+        &nbsp;
+        <Button 
+          primary={fioCallStatus === "none"} 
+          danger={fioCallStatus === "fail"} 
+          success={fioCallStatus === "ok"} 
+          disabled={fioCallStatus === "inprogress"}
+          onClick={() => callFioOneByOne()}
+        >
+          Run fio 1-by-1
+        </Button>
+        <Txt italic>Takes about 30 seconds (per call)</Txt>
+        {fioCallStatus === "inprogress" ? <ProgressBar value={fioProgress} /> : <></>}
         <ol style={{paddingBottom: '20vh'}}>
         {
           fioResults.map((r, i) => 
             <>
-              <li>
+              <li onClick={() => handleResultClick(`${r.disk_util && r.disk_util.length === 1 && r.disk_util[0].name}`)}>
                 <Txt>Name: {r.jobs[0].jobname} | Bandwith in kb/s </Txt>
-                <Table 
+                <Table
                   columns={[
                     {
                       field: 'bw_min',
