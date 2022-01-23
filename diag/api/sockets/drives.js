2a3c1b46d080dc001c9c68961042330344bcd040
Peter Makra <6892971+mcraa@users.noreply.github.com>
2022-01-23 14:45:47 +0100

    patch: [build] changed diag container to image from registry
    Change-Type: patch

--- diag/api/sockets/drives.js
+++
@@ -1,5 +0,0 @@
-var createWss = require('./socket')
-
-const drivesWs = createWss(7071);
-
-module.exports = drivesWs;
--- diag/start.sh
+++
@@ -1,7 +0,0 @@
-FILE=/usr/src/diag-data/startup.lock
-if [ -f "$FILE" ]; then
-    echo "$FILE exists, not starting service."
-    curl --header "Content-Type:application/json" "$BALENA_SUPERVISOR_ADDRESS/v2/applications/$BALENA_APP_ID/stop-service?apikey=$BALENA_SUPERVISOR_API_KEY" -d '{"serviceName": "diag-runner", "force": true}' 
-else 
-    node ./api/bin/www
-fi
