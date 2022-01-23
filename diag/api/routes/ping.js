3fc37be3df0b0e7bf5b43cc09d3740b3b6986ceb
Peter Makra <6892971+mcraa@users.noreply.github.com>
2022-01-23 14:45:47 +0100

    patch: [build] changed diag container to image from registry
    Change-Type: patch

--- diag/api/routes/ping.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET ping */
-router.get('/', function(req, res, next) {
-  res.sendStatus(200);
-});
-
-module.exports = router;
