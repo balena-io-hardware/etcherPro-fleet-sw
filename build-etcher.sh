8b03a780d3db2ab31d5bec3764513813292752cf
Peter Makra <6892971+mcraa@users.noreply.github.com>
2022-12-26 17:19:19 +0100

    patch: rebased to new build process

--- build-etcher.sh
+++ build-etcher.sh
@@ -3,9 +3,6 @@ git clone --recurse-submodules --depth 1 https://github.com/balena-io/etcher
 cd etcher
 export NODE_OPTIONS="--max-old-space-size=1024"
 export PUPPETEER_SKIP_DOWNLOAD=1
-sed -i.bak 's/"electron": "12.2.3"/"electron": "^13.5.0"/g' package.json
-sed -i.bak 's/"spectron": "14.0.0"/"spectron": "15.0.0"/g' package.json
-sed -i.bak 's/"electron-builder": "22.14.13"/"electron-builder": "^23.0.9"/g' package.json
 npm install
 npm ci
 rm node_modules/usb/prebuilds/linux-arm*/node.napi.armv*.node
